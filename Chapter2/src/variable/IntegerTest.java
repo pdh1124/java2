70031a9647154e0ffe54e33cb51d0c5bfa0f5706
pdh1124 <pdw3231@naver.com>
2021-02-01 21:02:51 +0900

    자바 강의 - 정수 자료형

---
+++ Chapter2/src/variable/IntegerTest.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class IntegerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte bs = -127;
+		
+		System.out.println(bs);
+		
+		//int iVal = 12345678900;
+		long lVal = 2147483647;
+		long lVal1 = 9999999999L; //기준 이상값을때는 L을 넣어주는것이 좋다.
+	}
+
+}
