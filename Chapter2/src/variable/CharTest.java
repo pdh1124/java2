8655ad3feb6350b9797af8cf721280357730bc34
pdh1124 <pdw3231@naver.com>
2021-02-01 21:11:49 +0900

    자바 기초 - 문자 자료형

---
+++ Chapter2/src/variable/CharTest.java
@@ -0,0 +1,26 @@
+package variable;
+
+public class CharTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char ch = 'A'; //한글자
+		
+		System.out.println(ch);
+		System.out.println((int)ch);
+		
+		int ich = 66;
+		System.out.println(ich);
+		System.out.println((char)ich);
+		
+		char hangul = '\uAC00';
+		System.out.println(hangul);
+		
+		
+		char ch2 = '한';
+		System.out.println(ch2);
+		System.out.println((int)ch2);
+	}
+
+}
