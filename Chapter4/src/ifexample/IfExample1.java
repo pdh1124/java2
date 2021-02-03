a41d59d373da0ce1e5186288bcf8c243f8b54685
pdh1124 <pdw3231@naver.com>
2021-02-03 10:31:36 +0900

    자바 기초 - if문

---
+++ Chapter4/src/ifexample/IfExample1.java
@@ -0,0 +1,17 @@
+package ifexample;
+
+public class IfExample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char gender = 'M';
+		
+		if (gender == 'F') {
+			System.out.println("여성입니다.");
+		} else {
+			System.out.println("여성이 아닙니다.");
+		}
+	}
+
+}
