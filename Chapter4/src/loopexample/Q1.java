11c5ffd6515185c454fa94641c6fb2857e2dc28e
pdh1124 <pdw3231@naver.com>
2021-02-03 21:07:35 +0900

    자바 기초 - 별로 다이아몬드 찍기

---
+++ Chapter4/src/loopexample/Q1.java
@@ -0,0 +1,35 @@
+package loopexample;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		// 1. 다이아몬드를 출력하기
+		int fullLine = 9;
+		int line = fullLine/2 + 1;
+		int star = 1;
+		int line2 = line;
+		
+		for(int k =0; k < line; k++) {
+			for(int i = 0; i < line2; i++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < star; j++) {
+				System.out.print("*");
+			}
+			line2--;
+			star+=2;
+			System.out.println();
+		}
+		for(int k = 0; k < line-1; k++) {
+			for(int i = line2 + 2; i > 0; i--) {
+				System.out.print(" ");
+			}
+			for(int j = star - 4; j > 0; j--) {
+				System.out.print("*");
+			}
+			line2++;
+			star-=2;
+			System.out.println();
+		}
+	}
+}
