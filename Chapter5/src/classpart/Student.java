a428841b14f46b24dd6096bb8a0296a7c0af336a
pdh1124 <pdw3231@naver.com>
2021-02-04 23:00:37 +0900

    자바 기초 - 함수와 매서드

--- Chapter5/src/classpart/Student.java
+++ Chapter5/src/classpart/Student.java
@@ -1,5 +1,10 @@
 package classpart;
 
+	//class 는 대분자로 시작하면 좋고
+	//package는 소문자로 하는게 좋고
+	//변수,메서드는 소문자로 시작해서 중간단어가 바뀔때 대문자로 (낙타의 등(카멜로테이션)
+
+
 public class Student { //public이 들어간 클래스는 자바파일 하나당 하나밖에 들어갈 수 없다.
 
 		
@@ -10,6 +15,10 @@ public class Student { //public이 들어간 클래스는 자바파일 하나당 하나밖에 들어�
 
 	public void ShowStudentInfo(/*매개 변수*/) {
 		System.out.println(studentName +"," + address);
+	}
+	
+	public String getStudentName() {
+		return studentName;
 	}
 }
 
