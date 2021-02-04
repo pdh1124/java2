4dbe3617c822bff28375c26460500b0aef616fea
pdh1124 <pdw3231@naver.com>
2021-02-04 19:54:58 +0900

    자바 기초 - 객체지향 프로그래밍

--- Chapter5/src/classpart/Student.java
+++ Chapter5/src/classpart/Student.java
@@ -1,10 +1,18 @@
 package classpart;
 
-public class Student {
+public class Student { //public이 들어간 클래스는 자바파일 하나당 하나밖에 들어갈 수 없다.
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	public int studentID;
+	public String studentName;
+	public String address;
 
+
+	public void ShowStudentInfo(/*매개 변수*/) {
+		System.out.println(studentName +"," + address);
 	}
+}
 
+class ABCD { //새로운 클래스를 만들 수 있지만 public을 넣을 순 없다.
+	
 }
---
+++ Chapter5/src/classpart/studentTest.java
@@ -0,0 +1,19 @@
+package classpart;
+
+public class studentTest {
+	
+	public static void main(String[] args) {
+		
+		//Student.java안에 Student라는 클래스 이름으로 변수를 하나 선언 후
+		//new는 생성을 한다는 뜻, Student()는 생성자로 클래스 이름과 동일하고 ()괄호를 열고 닫는다.
+		//메모리에 생성이 됨
+		Student studentLee = new Student();
+		
+		studentLee.studentName = "이순신";
+		studentLee.address = "서울";
+		
+		//public void ShowStudentInfo() {}라는 사용할 수 있는 메소드
+		studentLee.ShowStudentInfo();
+		
+	}
+}
