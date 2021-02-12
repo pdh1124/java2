55b6023cb7e1e74fd620862f0e1f3207f0f7fbf0
pdh1124 <pdw3231@naver.com>
2021-02-12 21:29:52 +0900

    자바 기초 - 오버라이딩과 오버로딩

--- ProjectOOP/InheritanceApp.java
+++ ProjectOOP/InheritanceApp.java
@@ -7,25 +7,39 @@ class Cal{
 	public int sum(int v1,int v2) {
 		return v1+v2;
 	}
+	//Overloading(세이브 파일명이 전부 똑같은 이름으로 되어있는 상태이지만 실제로는 세이브된 내용은 다름)
+	public int sum(int v1,int v2, int v3) {
+		return v1+v2+v3;
+	}
 }
 
 //class Cal에 마이너스 기능을 넣고 싶다면 상속받은 클래스를 만들어 주면 됨
 //Cal 클래스를 확장해서 Cal클래스가 가지고 있는 모든 메소드와 변수를 상속받게 되는 클래스 Cal3가 된다.
 class Cal3 extends Cal {
-	
+	//어떻게 하면 자식 클래스가 부모클래스 가지고 있는 기능에 추가하거나 덮어쓰기 위해 사용
+	//마이너스 기능 추가
+	public int minus(int v1,int v2) {
+		return v1-v2;
+	}
+	//Cal이 가지고 있는 sum이라는 기능인 sum을 수정할 수 있다. (오버라이딩(Overriding)이라고 함)
+	//똑같은 형태의 메소드를 추가하고 수정함
+	public int sum(int v1,int v2) {
+		System.out.println("Cal3"); //구분을 위해
+		return v1+v2;
+	}
 }
-
-
-
 
 public class InheritanceApp {
 
 	public static void main(String[] args) {
 		Cal c = new Cal();
 		System.out.println(c.sum(2, 1));
+		System.out.println(c.sum(2, 1, 5));
 		
 		//클래스 Cal3에 상속받고 있는 클래스Cal있는 sum이라는 메소드를 사용할 수 있음
 		Cal3 c3 = new Cal3();
 		System.out.println(c3.sum(2,3));
+		System.out.println(c3.minus(5,3));
+		System.out.println(c3.sum(2,1));
 	}
 }
