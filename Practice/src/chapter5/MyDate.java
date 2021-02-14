cd34fc563e7aaa00ff6f22a87e02396c16bbd624
pdh1124 <pdw3231@naver.com>
2021-02-14 10:37:28 +0900

    날짜 계산

--- Practice/src/chapter5/MyDate.java
+++ Practice/src/chapter5/MyDate.java
@@ -1,100 +1,101 @@
 package chapter5;
 
-import java.util.Calendar;
-
 public class MyDate {
-
-	private int day;
-	private int month;
-	private int year;
-	private boolean isValid = true;
 	
-	//setter
+	//1. 변수 지정 (private : 보호)
+	private int day; //일
+	private int month; //월
+	private int year; //년
+	private boolean isValid = false;
+	
 	public MyDate(int day, int month, int year) {
+	
+		
 		setYear(year);
 		setMonth(month);
 		setDay(day);
 	}
 	
-	//getter
+	
 	public int getDay() {
 		return day;
 	}
-	
 	public void setDay(int day) {
 		switch(month) {
-			case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-				if (day <0 || day >31) {
-					isValid = false;
-				}
-				else {
-					this.day = day;
-				}
-				break;
-			case 4: case 6: case 9: case 11:
-				if (day <0 || day >30) {
-					isValid = false;
-				}
-				else {
-					this.day = day;
-				}
-				break;
-			case 2:
-				if (( ( year % 4 ==0 &&  year % 100 !=0 ) || year % 400 ==0)){  //윤년인 경우
-					if (day <0 || day >29) {
-						isValid = false;
-					} 
-					else {
-						this.day = day;
-					}
-				}
-				else {
-					if (day <0 || day >28) {
-						isValid = false;
-					} 
-					else {
-						this.day = day;
-					}
-				}
-				break;
-			default:
+		case 1:case 3:case 5:case 7:case 8:case 10:case 12:
+			if(day < 0 || day > 31) {
 				isValid = false;
+			}
+			else {
+				isValid = true;
+				this.day = day;
+			}
+		case 4: case 6: case 9: case 11:
+			if(day < 0 || day > 30) {
+				isValid = false;
+			}
+			else {
+				isValid = true;
+				this.day = day;
+			}
+		case 2:
+			if(( ( year % 4 ==0 &&  year % 100 !=0 ) || year % 400 ==0)) { //윤년 (2월 29일이 있는 해)
+				if(day < 0 || day > 29) {
+					isValid = false;
+				}
+				else {
+					isValid = true;
+					this.day = day;
+				}
+			}
+			else {
+				if(day < 0 || day > 28) {
+					isValid = false;
+				}
+				else {
+					isValid = true;
+					this.day = day;
+				}
+			}
 		}
 	}
 
+	
+	
 	public int getMonth() {
 		return month;
 	}
-
 	public void setMonth(int month) {
-		if( month <1 || month >12) {
+		if(month < 1 || month > 12) {
 			isValid = false;
-		}
-		else {
+			
+		} else {
+			isValid = true;
 			this.month = month;
 		}
 	}
-
+	
+	
 	public int getYear() {
 		return year;
 	}
-
 	public void setYear(int year) {
-		if (year > Calendar.getInstance().get(Calendar.YEAR)) {
+		if(year < 0) {
 			isValid = false;
-		}
-		else {
+		} else {
+			isValid = true;
 			this.year = year;
+		}
+		
+	}
+	
+	public void showInfo() {
+		if(isValid == true) {
+		System.out.println(year + "년 " + month + "월 " + day + "일, 유효한 날짜 입니다.");
+		} else {
+			System.out.println("유효하지 않는 날짜 입니다.");
 		}
 	}
 	
-	public String isValid() {
-		
-		if(isValid) {
-			return "유효한 날짜입니다.";
-		}
-		else {
-			return "유효하지 않은 날짜입니다.";
-		}
-	}
+	
 }
--- Practice/src/chapter5/MydateTest.java
+++ Practice/src/chapter5/MydateTest.java
@@ -1,13 +1,18 @@
 package chapter5;
 
-public class MydateTest {
+public class MyDateTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		MyDate date1 = new MyDate(31, 2, 2000);
-		System.out.println(date1.isValid());
-		MyDate date2 = new MyDate(2, 10, 2006);
-		System.out.println(date2.isValid());
+		
+		MyDate d1 = new MyDate(22,1,2020);
+		d1.showInfo();
+		MyDate d2 = new MyDate(29,2,2021);
+		d2.showInfo();
+		MyDate d3 = new MyDate(29,2,2020);
+		d3.showInfo();
+		MyDate d4 = new MyDate(28,2,1992);
+		d4.showInfo();
+
 	}
 
 }
