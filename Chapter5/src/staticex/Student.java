4684357a1b3cabe1670b5294ae38ed341d087063
pdh1124 <pdw3231@naver.com>
2021-02-14 20:02:58 +0900

    자바 기초 - static

--- Chapter5/src/staticex/Student.java
+++ Chapter5/src/staticex/Student.java
@@ -13,18 +13,21 @@ public class Student {
 
 	//학생이 생성될때마다 학생의 id를 자동으로 새로 부여하고 싶다.
 	//기준값이 필요한데 ex) 1000, 1001, 1002, 1003 ... 순으로 값이 증가
-	//모든 인스턴스가 하나의 메모리를 바라 보는 것을 static이라고 한다.
+	//모든 인스턴스가 하나의 메모리를 바라 보는 것을 static이라고 한다!!!!! => new가 계속 생성되어도 학생번호는 계속 증가해야 되기 때문에 
 	private static int serialNum = 1000; //계속 증가하는 것
 	private int studentID; //증가 된 값 하나만을 가져감
 	public String studentName;
 	public String address;
 
-	public Student(String name) {
-		studentName = name;
-		serialNum++;
-		studentID = serialNum;
+	//초기화
+	public Student(String name) { //학생이 1명 만들어 질때마다
+		studentName = name; //이름을 지정해 주고
+		serialNum++; //시리얼 넘버를 +1씩 넘겨 준다.
+		//모든 학생이 같은 메모리를 바라보기 때문에 같은 학번이 되기 때문에
+		studentID = serialNum;  //매번 증가되는 시리얼넘버가 각 인스턴스의 학번이 됨
 	}
 	
+	//초기화 2
 	public Student(int id,String name) {
 		studentID = id;
 		studentName = name;
@@ -33,10 +36,14 @@ public class Student {
 		studentID = serialNum;
 	}
 	
+	
 	public void showStudentInfo() {
 		System.out.println(studentName + "," + address);
 	}
 	
+	
+	//학생 정보는 변하면 안되기 때문에 setter는 만들어 주지 않고
+	//private으로 보호 되기 때문에 getter를 사용한다.
 	public String getStudentName() {
 		return studentName;
 	}
@@ -45,6 +52,8 @@ public class Student {
 		return studentID;
 	}
 
+	
+	//getter, setter를 생성하게 되면 static 메소드를 생성하게 됨
 	public static int getSerialNum() {
 		return serialNum;
 	}
--- Chapter5/src/staticex/StudentIDTest.java
+++ Chapter5/src/staticex/StudentIDTest.java
@@ -3,13 +3,16 @@ package staticex;
 public class StudentIDTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Lee 학생 생성
 		Student studentLee = new Student("Lee");
 		System.out.println(Student.getSerialNum());
 		
+		//Kim 학생 생성
 		Student studentKim = new Student("Kim");
 		System.out.println(Student.getSerialNum());
 		
+		//Park 학생 생성
 		Student studentPark = new Student("Park");
 		System.out.println(Student.getSerialNum());
 		System.out.println(studentKim.getStudentID()); //같은 메모리를 바라보고 있다.
@@ -18,5 +21,6 @@ public class StudentIDTest {
 		System.out.println(studentKim.getStudentID());
 		System.out.println(studentPark.getStudentID());
 	}
+	
 
 }
