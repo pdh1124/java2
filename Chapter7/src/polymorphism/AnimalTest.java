62dfd5556b7ea96a398e2457aa22fe4654a4c718
pdh1124 <pdw3231@naver.com>
2021-02-16 20:38:15 +0900

    자바 기초 - 다운캐스팅과 instanceof

--- Chapter7/src/polymorphism/AnimalTest.java
+++ Chapter7/src/polymorphism/AnimalTest.java
@@ -13,7 +13,7 @@ class Human extends Animal {
 		System.out.println("사람이 두발로 걷습니다.");
 	}
 	
-	public void readBooks() {
+	public void readBooks() { //각 클래스마다만 있는 메소드
 		System.out.println("사람이 책을 읽습니다.");
 	}
 }
@@ -22,11 +22,18 @@ class Tiger extends Animal {
 	public void move() {
 		System.out.println("호랑이가 네 발로 뜁니다.");
 	}
+	public void hunting() { //각 클래스마다만 있는 메소드
+		System.out.println("호랑이가 사냥을 합니다.");
+	}
+	
 }
 
 class Eagle extends Animal {
 	public void move() {
 		System.out.println("독수리가 하늘을 날아갑니다.");
+	}
+	public void flying() { //각 클래스마다만 있는 메소드
+		System.out.println("독수리가 날개를 쭉 펴고 멀리 날아갑니다.");
 	}
 }
 
@@ -46,27 +53,69 @@ public class AnimalTest {
 		Animal tAnimal = new Tiger();
 		Animal eAnimal = new Eagle();
 		
-		AnimalTest test = new AnimalTest();
+		Human human = (Human)hAnimal; //다운캐스팅을 해야 각각의 클래스마다 있는 메소드를 사용 가능하게 함
+		human.readBooks();
 		
+		/* instanceof는 오류 확인 법으로 다운캐스팅을 할때 자바가 오류를 잘 못잡음으로 한번 입력해서 오류가 있는지 확인하기
+		if(hAnimal instanceof Human) {
+			Human human = (Human)hAnimal;
+			human.readBooks();
+		}
+		*/
+		
+		System.out.println("---------------");	
+		
+		AnimalTest test = new AnimalTest();
 		test.moveAnimal(hAnimal);
 		test.moveAnimal(tAnimal);
 		test.moveAnimal(eAnimal);
-		
-		
-		
+			
 		
 		
 		ArrayList<Animal> animalList = new ArrayList<Animal>();
-		
 		animalList.add(hAnimal);
 		animalList.add(tAnimal);
 		animalList.add(eAnimal);
 		
+		System.out.println("---------------");
+		
+		test.testDownCasting(animalList);
+		
+		System.out.println("---------------");
+		
 		for(Animal animal : animalList) {
 			animal.move();
 		}
+		
 		// hAnimal.readBooks(); 실행이 안됨. 실행을 하려면 원래의 타입으로 돌아가야 함(다운캐스팅)
+		
+		
 	}
+	
+	public void testDownCasting(ArrayList<Animal> list) {
+		for(int i=0; i<list.size(); i++) {
+			Animal animal = list.get(i);
+			
+			if( animal instanceof Human) {
+				Human human = (Human)animal;
+				human.readBooks();
+			}
+			else if( animal instanceof Tiger) {
+				Tiger tiger = (Tiger)animal;
+				tiger.hunting();
+			}
+			else if( animal instanceof Eagle) {
+				Eagle eagle = (Eagle)animal;
+				eagle.flying();
+			} 
+			else {
+				System.out.println("ERROR");
+			}
+		}
+		
+		
+	}
+	
 	
 	
 	
