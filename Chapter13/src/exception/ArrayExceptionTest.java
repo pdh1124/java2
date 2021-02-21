610ebe6d66baf7915e70362a946775727dc6d037
pdh1124 <pdw3231@naver.com>
2021-02-21 23:11:20 +0900

    자바 기초 - 예외처리

---
+++ Chapter13/src/exception/ArrayExceptionTest.java
@@ -0,0 +1,24 @@
+package exception;
+
+public class ArrayExceptionTest {
+
+	public static void main(String[] args) {
+		
+		//강제적으로 error를 발생 시킴
+		//int 형식의 array를 5개짜리로 만듬
+		int[] arr = new int[5];
+		
+		try {
+		//배열이 5개인데 0~5까지면 6개를 찍어야 하는데 하나가 모자르면 에러가 생김
+		for(int i=0; i <= 5; i++) {
+			System.out.println(arr[i]);
+			}
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println(e);
+			System.out.println("예외처리");
+		}
+		System.out.println("프로그램 종료");
+
+	}
+
+}
