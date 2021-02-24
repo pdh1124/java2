334188f1f2b4f0cebfbfa6feee3c2d41c8f6e2a1
pdh1124 <pdw3231@naver.com>
2021-02-24 17:15:57 +0900

    자바 기초 - 기록

--- Chapter14/src/stream/coffee/Coffee.java
+++ Chapter14/src/stream/coffee/Coffee.java
@@ -1,5 +1,10 @@
 package stream.coffee;
 
+//아메리카노
+//라떼 = 아메리카노 + 우유
+//모카커피 = 아메리카노 + 우유 + 모카시럽
+//whipping cream 모카 커피 = 아메리카노 + 우유 + 모카시럽 + whipping cream
+
 //사용할 클래스가 아니라서 추상클래스(abstract)로 만듦
 public abstract class Coffee {
 	
